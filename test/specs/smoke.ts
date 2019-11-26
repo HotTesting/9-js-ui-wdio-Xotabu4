00744b98d1a8ecbc758eff7e35a741e1388a2211
Oleksandr Khotemskyi <xotabu4@gmail.com>
2019-11-26 23:13:54 +0200

    added hometask 3 lesson

--- test/specs/smoke.ts
+++ test/specs/smoke.ts
@@ -1,10 +1,25 @@
 describe('Website', function () {
     it('should be alive', function () {
         browser.url(`/`)
-        const img = $('img[src="http://ip-5236.sunline.net.ua:38015/images/logotype.png"]')
+        const img = $('img[src$="images/logotype.png"]')
+
         
         if(!img.isExisting()) {
             throw new Error('Website should be opened, and logo displayed');
         }
     })
+
+    it('test', function () {
+        browser.url(`/`)
+
+        const popularProducts = $$('#box-popular-products .product')
+
+        popularProducts[2].$('.name')
+
+
+        $('=Change').click()
+
+        $('=Purple Duck').$('../..') // find element 2 levels up from Purple Duck
+    })
 })
+
