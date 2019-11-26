c3ec79fb94b727691c5a94e6c783f75169c1108e
Oleksandr Khotemskyi <xotabu4@gmail.com>
2019-11-26 19:38:03 +0200

    Added typescript support

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,5 @@
+require("ts-node").register({ files: true });
+
 exports.config = {
     //
     // ====================
@@ -20,7 +22,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/specs/**/*.js'
+        './test/specs/**/*.ts'
     ],
     // Patterns to exclude.
     exclude: [
